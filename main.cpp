b294ede7d28100509b733d8f82d831b931aaff50
S5201967 <91668770+S5201967@users.noreply.github.com>
2021-12-01 17:27:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,15 @@
 #include <iostream>
-
-int main(){
-  return 0;
+#include "lib.h"
+using namespace std;
+int main() {
+   char a;
+   cin>>a;
+   int risultato=verifica(a);
+   if(risultato){
+       cout<<convert(a)<<endl;
+   }else{
+       cout<<"errore";
+   }
+    return 0;
 }
+
